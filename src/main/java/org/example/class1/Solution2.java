ab951beacfd5c75949b30a2bf4ab138083fff7a3
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-04-17 14:24:03 +0900

    2. 청소 문제 정답

--- src/main/java/org/example/class1/Solution2.java
+++ src/main/java/org/example/class1/Solution2.java
@@ -4,8 +4,26 @@ import java.util.*;
 public class Solution2 {
     public int[] solution(int[][] board, int k){
         int[] answer = new int[2];
-
-
+        int n = board.length;
+        int[] dx = {-1,0,1,0};
+        int[] dy = {0,1,0,-1};
+        int d = 1;
+        int x=0;
+        int y=0;
+        int count = 0;
+        while(count<k) {
+            count ++;
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if(nx<0 || nx>=n || ny<0 || ny>=n || board[nx][ny]==1) {
+                d = (d+1)%4;
+                continue;
+            }
+            x = nx;
+            y = ny;
+        }
+        answer[0] = x;
+        answer[1] = y;
 
         return answer;
     }
