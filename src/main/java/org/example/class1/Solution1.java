4cc8b5935d177aaee58531f7dc80aa2e6e46295d
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-04-17 14:02:10 +0900

    1. 사다리 타기 정답

--- src/main/java/org/example/class1/Solution1.java
+++ src/main/java/org/example/class1/Solution1.java
@@ -4,10 +4,19 @@ import java.util.Arrays;
 
 public class Solution1 {
 
-        public char[] solution(int n, int[][] ladder){
+        public char[] solution(int n, int[][] ladder) {
             char[] answer = new char[n];
 
-
+            for (int i = 0; i < n; i++) {
+                answer[i] = (char) (i + 65);  //답에 알파벳 넣기
+            }
+            for (int[] line : ladder) {
+                for (int x : line) {
+                    char tmp = answer[x];
+                    answer[x] = answer[x - 1];
+                    answer[x-1] = tmp;
+                }
+            }
             return answer;
         }
         public static void main(String[] args){
@@ -17,5 +26,5 @@ public class Solution1 {
             System.out.println(Arrays.toString(T.solution(8, new int[][]{{1, 5}, {2, 4, 7}, {1, 5, 7}, {2, 5, 7}})));
             System.out.println(Arrays.toString(T.solution(12, new int[][]{{1, 5, 8, 10}, {2, 4, 7}, {1, 5, 7, 9, 11}, {2, 5, 7, 10}, {3, 6, 8, 11}})));
         }
-    }
+
 }
