34c0a392e513cd42036278330508a08f9e368cf0
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-04-17 11:49:16 +0900

    1. 사다리타기 정답

--- src/main/java/org/example/class1/Solution1.java
+++ src/main/java/org/example/class1/Solution1.java
@@ -7,6 +7,18 @@ public class Solution1 {
     public char[] solution(int n, int[][] ladder){
         char[] answer = new char[n];
 
+        for(int i=0; i<n; i++){
+            answer[i] = (char)(i+65);
+        }
+
+        for(int[] line : ladder) {
+            for(int x : line) {
+                char tmp = answer[x];
+                answer[x] = answer[x-1];
+                answer[x-1] = tmp;
+            }
+        }
+
         return answer;
     }
 
