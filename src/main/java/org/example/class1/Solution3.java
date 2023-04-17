6bf013da1989bb7189d7614090d25bd2d4ac08cf
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-04-17 14:40:15 +0900

    3. 집나간 강아지 - 현수와 강아지 시작 위치 파악하기

--- src/main/java/org/example/class1/Solution3.java
+++ src/main/java/org/example/class1/Solution3.java
@@ -5,6 +5,28 @@ class Solution3 {
     public int solution(int[][] board){
         int answer = 0;
 
+        int h_x = 0;
+        int h_y = 0;
+        int d_x = 0;
+        int d_y = 0;
+
+        int n = board.length;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(board[i][j]==2){
+                    h_x = i;
+                    h_y = j;
+                }
+                if(board[i][j]==3){
+                    d_y = j;
+                    d_x = i;
+                }
+            }
+        }
+        System.out.println("h_x = " + h_x + " h_y = " + h_y);
+        System.out.println("d_x = " + d_x + " d_y = " + d_y);
+
+
         return answer;
     }
 
