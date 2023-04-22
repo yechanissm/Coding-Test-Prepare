0a4617296b82f256e26de9f2aff2c30614c56142
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-04-22 19:11:46 +0900

    정답 - 문제 오류 원인(answer배열의크기를 초기화 하지 않았음)

--- src/main/java/org/example/class1/등수매기기.java
+++ src/main/java/org/example/class1/등수매기기.java
@@ -7,8 +7,9 @@ public class 등수매기기 {
     public static void main(String[] args) {
         int[][] score = {{80,70}, {90,50}, {40,70}};
 
-        int[] answer = {};
+
         int[] sum_array = new int[score.length];
+        int[] answer = new int[sum_array.length];
 
         //우선 합을 먼저 계산해야 한다. 그걸 배열로 만들어보자.
         for(int i=0; i<score.length; i++) {
