8902e39f9702a1358d9610c3e1820f7942089ebc
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-04-22 19:05:03 +0900

    등수 매기기 문제 시작

---
+++ src/main/java/org/example/class1/등수매기기.java
@@ -0,0 +1,15 @@
+package org.example.class1;
+
+import java.util.Arrays;
+
+public class 등수매기기 {
+
+    public static void main(String[] args) {
+        int[][] score = {{80,70}, {90,50}, {40,70}};
+
+        int[] answer = {};
+        int[] sum_array = new int[score.length];
+
+
+    }
+}
