126584516080d6d453d53ebab7244f7e42d88a61
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-04-22 19:06:18 +0900

    평균을 매긴 배열 생성

--- src/main/java/org/example/class1/등수매기기.java
+++ src/main/java/org/example/class1/등수매기기.java
@@ -10,6 +10,10 @@ public class 등수매기기 {
         int[] answer = {};
         int[] sum_array = new int[score.length];
 
-
+        //우선 합을 먼저 계산해야 한다. 그걸 배열로 만들어보자.
+        for(int i=0; i<score.length; i++) {
+            sum_array[i] = (score[i][0] + score[i][1])/2;
+        }
+        System.out.println("sum_array = " + Arrays.toString(sum_array));
     }
 }
