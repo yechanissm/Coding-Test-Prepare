0a2db7a118e564094dc1db8be150538214657eb8
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-04-23 01:24:33 +0900

    정답 - 소인수 2와 5만 남은 경우

--- src/main/java/org/example/level0/유한소수판별하기.java
+++ src/main/java/org/example/level0/유한소수판별하기.java
@@ -2,26 +2,34 @@ package org.example.level0;
 
 public class 유한소수판별하기 {
 
-    public static int solution(int a, int b) {
-        int answer = 0;
-
-        for(int i=1; i<b; i++) {
-            if(a % i == 0 ) {
-                if (b % (a/i) == 0) {
-                    return 1;
-                }
-            }
-
-        }
-        return 2;
-
+    //유클리드 호재법!!최대공약수 구하기
+    static int gcd(int a, int b) {
+       if(b == 0) {
+           return a;
+       } else {
+           return gcd(b, a%b);
+       }
     }
-
+    public static int solution(int a, int b) {
+        int B = b / gcd(a,b);
+        while( B != 1) {
+            if(B%2 == 0) {
+                B /= 2;
+            }
+            else if(B % 5 == 0) {
+                b /= 5;
+            }
+            else {
+                return 2;
+            }
+        }
+        return 1;
+    }
     public static void main(String[] args) {
-
         System.out.println(solution(7,20));
         System.out.println(solution(11,22));
         System.out.println(solution(12,21));
+
     }
 
 }
