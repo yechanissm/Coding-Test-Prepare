d7b623064fcf53bb792baece3808fa48f664ef18
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-02 18:01:07 +0900

    주어진 배열의 최솟값 구하기

--- src/main/java/org/example/level0/제일작은수제거하기.java
+++ src/main/java/org/example/level0/제일작은수제거하기.java
@@ -1,9 +1,13 @@
 package org.example.level0;
 
+import java.util.Arrays;
+
 public class 제일작은수제거하기 {
 
     static int[] solution(int[] arr){
-
+        int[] temp = arr.clone();
+        Arrays.sort(temp);
+        int min = temp[0]; //최소값 구하기
     }
 
     public static void main(String[] args) {
