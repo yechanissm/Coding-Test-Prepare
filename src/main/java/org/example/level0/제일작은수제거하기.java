6e59adb82e7dd2d07860516c6ede25a914a71d52
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-02 18:16:50 +0900

    정답 - 제일 작은 수 제거하기

--- src/main/java/org/example/level0/제일작은수제거하기.java
+++ src/main/java/org/example/level0/제일작은수제거하기.java
@@ -6,33 +6,38 @@ import java.util.List;
 
 public class 제일작은수제거하기 {
 
-    static int[] solution(int[] arr){
+    static int[] solution(int[] arr) {
         int[] temp = arr.clone();
         Arrays.sort(temp);
         int min = temp[0]; //최소값 구하기
 
         List<Integer> list = new ArrayList<>();
-        for(int i=0; i<arr.length; i++) {
-            if(arr[i]!=min) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != min) {
                 list.add(arr[i]);
             }
         }
         //최소값을 제외한 값들을 리스트에 넣기
         int[] answer;
-        if(list.isEmpty()) {
-            answer = new int[1] {-1};
+        if (list.isEmpty()) {
+            answer = new int[1];
+            answer[0] = -1;
         }
         else {
             answer = new int[list.size()];
-            for(int i=0; i<list.size(); i++) {
+            for (int i = 0; i < list.size(); i++) {
                 answer[i] = list.get(i);
             }
         }
         //리스트가 비어있으면 -1 아니면 값을 넣어준다.
+        return answer;
     }
 
     public static void main(String[] args) {
-        System.out.println(solution({4,3,2,1));
-        System.out.println(solution({10}));
+        int[] arr = {4,3,2,1};
+        int[] arr2 = {10};
+        System.out.println(Arrays.toString(solution(arr)));
+        System.out.println(Arrays.toString(solution(arr2)));
+
     }
 }
