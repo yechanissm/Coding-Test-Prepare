0c6dec72ad3f4a9a3923d1b9710156b42fac4065
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-02 17:59:38 +0900

    배열에서 제일 작은 수 제거하기 문제

---
+++ src/main/java/org/example/level0/제일작은수제거하기.java
@@ -0,0 +1,13 @@
+package org.example.level0;
+
+public class 제일작은수제거하기 {
+
+    static int[] solution(int[] arr){
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution({4,3,2,1));
+        System.out.println(solution({10}));
+    }
+}
---
+++ src/main/java/org/example/memo.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+public class memo {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        int[] a = {1,23,4,5};
+        int[] clone = a.clone();
+    }
+}
