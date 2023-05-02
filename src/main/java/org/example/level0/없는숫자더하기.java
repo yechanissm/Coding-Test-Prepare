e9fbc29770aa1adea83d0f0abd3051de586f9c11
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-02 19:38:01 +0900

    같은 게 있는지 확인하는 boolean 메소드

--- src/main/java/org/example/level0/없는숫자더하기.java
+++ src/main/java/org/example/level0/없는숫자더하기.java
@@ -1,15 +1,21 @@
 package org.example.level0;
 
+import java.util.Arrays;
+
 public class 없는숫자더하기 {
     static int solution(int[] numbers){
         int answer = 0;
-
-
+        for(int i=1; i<=9; i++) {
+            boolean result = checking(i, numbers);
+        }
         return answer;
     }
+
 
     public static void main(String[] args) {
         int[] numbers1 = {1,2,3,4,6,7,8,0};
         int[] numbers2 = {5,8,4,0,6,7,9};
+        int solution = solution(numbers1);
+        System.out.println(solution);
     }
 }
