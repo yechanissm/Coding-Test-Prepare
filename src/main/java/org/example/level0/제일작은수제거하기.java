433d752cdd480de59a038ea00442302db26455c8
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-02 18:03:06 +0900

    최소값이 아닌 값들 리스트에 넣기

--- src/main/java/org/example/level0/제일작은수제거하기.java
+++ src/main/java/org/example/level0/제일작은수제거하기.java
@@ -1,6 +1,8 @@
 package org.example.level0;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class 제일작은수제거하기 {
 
@@ -8,6 +10,16 @@ public class 제일작은수제거하기 {
         int[] temp = arr.clone();
         Arrays.sort(temp);
         int min = temp[0]; //최소값 구하기
+
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i]!=min) {
+                list.add(arr[i]);
+            }
+        }
+        //최소값을 제외한 값들을 리스트에 넣기
+
+
     }
 
     public static void main(String[] args) {
