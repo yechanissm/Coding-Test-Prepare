c0452622598d0405a219a894a5d2a880ade27551
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-03 15:05:18 +0900

    가운데 글자 가져오기 문제

---
+++ src/main/java/org/example/level0/가운데글자가져오기.java
@@ -0,0 +1,18 @@
+package org.example.level0;
+
+public class 가운데글자가져오기 {
+
+    static String solution(String s) {
+        String answer = "";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "abcde";
+        String s2 = "qwer";
+
+        System.out.println(solution(s1));
+        System.out.println(solution(s2));
+    }
+}
