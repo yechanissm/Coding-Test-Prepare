f8d11789dfba9f6865c2e372c8dc01160c3d45d8
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-03 17:52:00 +0900

    정답 - 가운데 글자 가져오기

--- src/main/java/org/example/level0/가운데글자가져오기.java
+++ src/main/java/org/example/level0/가운데글자가져오기.java
@@ -4,7 +4,16 @@ public class 가운데글자가져오기 {
 
     static String solution(String s) {
         String answer = "";
-
+        int len = s.length();
+        if (len%2!=0) {
+            char c = s.charAt(len / 2);
+            answer += String.valueOf(c);
+        }
+        else {
+            char c1 = s.charAt(len/2-1);
+            char c2 = s.charAt(len/2);
+            answer += String.valueOf(c1) + String.valueOf(c2);
+        }
         return answer;
     }
 
