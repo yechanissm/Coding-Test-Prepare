d1ecce1b8bcb5b5508e9bc9f70ee7cf1907d7b80
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-06 14:54:26 +0900

    코드 작성 - 2차원배열 값 오류

--- src/main/java/org/example/level0/행렬의덧셈.java
+++ src/main/java/org/example/level0/행렬의덧셈.java
@@ -4,7 +4,11 @@ public class 행렬의덧셈 {
 
     static int[][] solution(int[][] arr1, int[][]arr2) {
         int [][] answer = new int[arr1.length][arr1.length];
-
+        for(int i=0; i< arr1.length; i++) {
+            for(int j=0; i<arr1[0].length; i++) {
+                answer[i][j] = arr1[i][j] +  arr2[i][j];
+            }
+        }
         return answer;
     }
 
