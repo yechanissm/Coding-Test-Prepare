133aa1722675cf59e2901097c3591d5f84fe065c
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-06 14:49:04 +0900

    행렬의 덧셈

---
+++ src/main/java/org/example/level0/행렬의덧셈.java
@@ -0,0 +1,16 @@
+package org.example.level0;
+
+public class 행렬의덧셈 {
+
+    static int[][] solution(int[][] arr1, int[][]arr2) {
+        int [][] answer = new int[arr1.length][arr1.length];
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr1 = {{1,2}, {2,3}};
+        int[][] arr2 = {{3,4}, {5,6}};
+        System.out.println(solution(arr1, arr2));
+    }
+}
