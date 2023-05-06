74e847b155a5135506e5906d5f2852bb8019b369
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-06 11:30:05 +0900

    StringBuilder를 활용하였으나 테스트 실패

--- src/main/java/org/example/level0/문자열내림차순으로배치하기.java
+++ src/main/java/org/example/level0/문자열내림차순으로배치하기.java
@@ -3,11 +3,13 @@ package org.example.level0;
 public class 문자열내림차순으로배치하기 {
     static String solution(String s ){
         String answer = "";
-
+        StringBuilder sb = new StringBuilder(s);
+        StringBuilder reverse = sb.reverse();
+        answer = reverse.toString();
         return answer;
     }
     public static void main(String[] args) {
         String s = "Zbcdefg";
-
+        System.out.println(solution(s));
     }
 }
