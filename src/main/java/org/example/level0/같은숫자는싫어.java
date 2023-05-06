ffefd5c2c5d794af8bcfbcc8d73ec045a27f2206
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-06 21:58:55 +0900

    같은 숫자는 싫어

---
+++ src/main/java/org/example/level0/같은숫자는싫어.java
@@ -0,0 +1,18 @@
+package org.example.level0;
+
+import java.util.Arrays;
+
+public class 같은숫자는싫어 {
+
+    static int[] solution(int[] arr) {
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1,1,3,3,0,1,1};
+        int[] arr2 = {4,4,4,3,3};
+
+        System.out.println(Arrays.toString(solution(arr1)));
+        System.out.println(Arrays.toString(solution(arr2)));
+    }
+}
