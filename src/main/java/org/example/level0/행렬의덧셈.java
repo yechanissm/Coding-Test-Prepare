40bb779982b932bbc3827ebe641765464d78695e
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-06 15:00:27 +0900

    정답 - 행렬의 덧셈 (오류 : 이중 for문 잘못된 i, j 사용)

--- src/main/java/org/example/level0/행렬의덧셈.java
+++ src/main/java/org/example/level0/행렬의덧셈.java
@@ -3,18 +3,20 @@ package org.example.level0;
 public class 행렬의덧셈 {
 
     static int[][] solution(int[][] arr1, int[][]arr2) {
-        int [][] answer = new int[arr1.length][arr1.length];
+        int [][] answer = new int[arr1.length][arr1[0].length];
         for(int i=0; i< arr1.length; i++) {
-            for(int j=0; i<arr1[0].length; i++) {
+            for(int j=0; j<arr1[0].length; j++) {
                 answer[i][j] = arr1[i][j] +  arr2[i][j];
             }
         }
+        System.out.println(arr1[0].length);
         return answer;
     }
 
     public static void main(String[] args) {
         int[][] arr1 = {{1,2}, {2,3}};
         int[][] arr2 = {{3,4}, {5,6}};
+        int[][] arr3 = {{1}, {2}};
         System.out.println(solution(arr1, arr2));
     }
 }
