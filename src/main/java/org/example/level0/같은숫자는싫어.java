8b2c355415b2e9d1733500bfd82eed08176b08bd
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-06 22:01:50 +0900

    리스트에 같은 숫자가 아닌 숫자 넣기

--- src/main/java/org/example/level0/같은숫자는싫어.java
+++ src/main/java/org/example/level0/같은숫자는싫어.java
@@ -1,10 +1,21 @@
 package org.example.level0;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class 같은숫자는싫어 {
 
     static int[] solution(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        int tmp = -1;
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] != tmp) {
+                list.add(arr[i]);
+                tmp = arr[i];
+            }
+        }
+
 
     }
 
