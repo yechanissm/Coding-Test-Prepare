450a57429fee7d61452aa08ac3e5a3d9e2719763
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-06 11:43:06 +0900

    약수의 개수와 덧셈

---
+++ src/main/java/org/example/level0/약수의개수와덧셈.java
@@ -0,0 +1,15 @@
+package org.example.level0;
+
+public class 약수의개수와덧셈 {
+    static int solution(int left, int right) {
+        int answer = 0;
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        int left = 13;
+        int right = 17;
+
+        System.out.println(solution(left, right));
+    }
+}
