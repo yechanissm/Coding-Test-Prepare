ff53cef348a172041a9d160d537f5e6fc40f94ce
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-06 11:45:53 +0900

    코드 실행 - 정답 오류

--- src/main/java/org/example/level0/약수의개수와덧셈.java
+++ src/main/java/org/example/level0/약수의개수와덧셈.java
@@ -3,7 +3,21 @@ package org.example.level0;
 public class 약수의개수와덧셈 {
     static int solution(int left, int right) {
         int answer = 0;
-
+        int count = 0;
+        for(int i=left; i<=right; i++) {
+            for(int j=1; j<=left; j++) {
+                if(left%i==0) {
+                    count++;
+                }
+            }
+            if(count%2==0) {
+                answer += left;
+            }
+            else{
+                answer -= left;
+            }
+            count =0;
+        }
         return answer;
     }
     public static void main(String[] args) {
