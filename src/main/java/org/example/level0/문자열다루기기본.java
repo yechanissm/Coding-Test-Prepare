7fcf40d5c97c3b76f01ca416b07755d66b6ba92c
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-06 13:23:33 +0900

    문자열 다루기 기본 문제

---
+++ src/main/java/org/example/level0/문자열다루기기본.java
@@ -0,0 +1,18 @@
+package org.example.level0;
+
+public class 문자열다루기기본 {
+
+    static boolean solution(String s) {
+        boolean answer = false;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "a234";
+        String s2 = "1234";
+
+        System.out.println("s1 = " + solution(s1));
+        System.out.println("s2 = " + solution(s2));
+    }
+}
