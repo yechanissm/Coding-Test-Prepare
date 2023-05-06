b0620b5f63ef219d5868ffceee8f745f9b00ef7f
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-06 11:39:27 +0900

    정답 - 문자열 내림차순으로 배치하기

--- src/main/java/org/example/level0/문자열내림차순으로배치하기.java
+++ src/main/java/org/example/level0/문자열내림차순으로배치하기.java
@@ -1,13 +1,19 @@
 package org.example.level0;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class 문자열내림차순으로배치하기 {
     static String solution(String s ){
         String answer = "";
-        StringBuilder sb = new StringBuilder(s);
-        StringBuilder reverse = sb.reverse();
-        answer = reverse.toString();
+        String[] split = s.split("");
+        Arrays.sort(split, Comparator.reverseOrder());
+        for(String ss: split) {
+            answer+=ss;
+        }
         return answer;
     }
+    //먼저 나누고, 정렬한다음 내림차순으로, Comparator.reverseOrder() 사용
     public static void main(String[] args) {
         String s = "Zbcdefg";
         System.out.println(solution(s));
