0d63fc8b6441daf814238912198720824d405a09
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-06 11:21:38 +0900

    내적 문제

---
+++ src/main/java/org/example/level0/내적.java
@@ -0,0 +1,15 @@
+package org.example.level0;
+
+public class 내적{
+    static int solution(int[] a, int[] b) {
+        int answer = 0;
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        int[] a = {1,2,3,4};
+        int[] b = {-3,-1,0,2};
+
+        System.out.println(solution(a,b));
+    }
+}
