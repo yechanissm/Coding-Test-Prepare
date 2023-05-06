87e5d2f564c4ef634402c29297a5f6678632f014
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-06 15:17:42 +0900

    f최대 공약수와 최소공배수 문제

---
+++ src/main/java/org/example/level0/최대공약수와최소공배수.java
@@ -0,0 +1,18 @@
+package org.example.level0;
+
+public class 최대공약수와최소공배수 {
+
+    static int[] solution(int n, int m) {
+        int[] answer = new int[2];
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        int n =3;
+        int m = 12;
+        int n1 = 2;
+        int n2 = 5;
+        System.out.println(solution(n,m));
+        System.out.println(solution(n1,n2));
+    }
+}
