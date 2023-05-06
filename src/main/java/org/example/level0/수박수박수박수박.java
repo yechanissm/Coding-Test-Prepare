0d6254b50eec7f9cc5e0dbef3343e8af630e72d4
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-06 11:10:10 +0900

    수박수박수박수박수박수? 문제

---
+++ src/main/java/org/example/level0/수박수박수박수박.java
@@ -0,0 +1,16 @@
+package org.example.level0;
+
+public class 수박수박수박수박 {
+    static String solution(int n) {
+        String answer = "";
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int n1 = 3;
+        int n2 = 4;
+
+        System.out.println(solution(n1));
+        System.out.println(solution(n2));
+    }
+}
