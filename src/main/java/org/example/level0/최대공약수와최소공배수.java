418a40080f95ab2b915c57201e9ec9b55af5d3c2
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-06 15:20:57 +0900

    유클리드 호재법 - 최대공약수 구하기

--- src/main/java/org/example/level0/최대공약수와최소공배수.java
+++ src/main/java/org/example/level0/최대공약수와최소공배수.java
@@ -1,11 +1,19 @@
 package org.example.level0;
 
 public class 최대공약수와최소공배수 {
-
+    static int gcd(int big, int small) {
+        if(big%small==0) {
+            return small;
+        }
+        return gcd(small,big%small);
+    }
     static int[] solution(int n, int m) {
         int[] answer = new int[2];
         int big = Math.max(n,m);
         int small = Math.min(n,m);
+
+        answer[0] = gcd(big,small);
+
         return answer;
     }
     public static void main(String[] args) {
