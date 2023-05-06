7528e77aeb64522c00bd0a3c4464773dcb3bebac
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-06 21:43:16 +0900

    정답 - 3진법 뒤집기

--- src/main/java/org/example/level0/삼진법뒤집기.java
+++ src/main/java/org/example/level0/삼진법뒤집기.java
@@ -4,12 +4,19 @@ public class 삼진법뒤집기 {
 
     static int solution(int n) {
         int answer =0;
-
+        String sss = "";
+        while(n>0) {
+            sss += n%3;
+            n /=3;
+        }
+        answer = Integer.parseInt(sss, 3);
         return answer;
     }
 
     public static void main(String[] args) {
         int n1 =45;
         int n2 = 125;
+        System.out.println(solution(n1));
+        System.out.println(solution(n2));
     }
 }
