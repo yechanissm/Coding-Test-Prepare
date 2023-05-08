bef324be6880b6e42ef96716d1581681eb07b1ad
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-08 12:44:03 +0900

    정답 제출 - 오류

--- src/main/java/org/example/level0/이상한문자만들기.java
+++ src/main/java/org/example/level0/이상한문자만들기.java
@@ -4,7 +4,23 @@ public class 이상한문자만들기 {
 
     static String solution(String s) {
         String answer = "";
-
+        String[] words = s.split(" ");
+        for (String word : words) {
+            for(int i=0; i<word.length(); i++) {
+                if(i%2==0) {
+                    char c = word.charAt(i);
+                    char c1 = Character.toUpperCase(c);
+                    answer+=c1;
+                }
+                else{
+                    char c = word.charAt(i);
+                    char c1 = Character.toLowerCase(c);
+                    answer+=c1;
+                }
+            }
+            answer += " ";
+        }
+        answer = answer.trim();
         return answer;
     }
 
