5e0247d9680955c7077cb4e825b4d6f5ddd4b652
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-08 17:20:47 +0900

    바이너리 배열을 #과 띄워쓰기로 치환

--- src/main/java/org/example/level0/비밀지도1차.java
+++ src/main/java/org/example/level0/비밀지도1차.java
@@ -5,10 +5,21 @@ public class 비밀지도1차 {
     static String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = {};
         String[] binary_arr1 = binaryChange(arr1,n);
-        for(String s : binary_arr1) {
-            System.out.println(s);
-        }
+        String[] binary_arr2 = binaryChange(arr2,n);
+
+        String[] change_arr1 = replaceChange(binary_arr1);
+
+
         return answer;
+    }
+
+    static String[] replaceChange(String[] binary_arr1) {
+        String[] result = new String[binary_arr1.length];
+        for(int i=0; i<binary_arr1.length; i++) {
+            result[i] = binary_arr1[i].replace("1", "#");
+            result[i] = result[i].replace("0", " ");
+        }
+        return result;
     }
 
     static String[] binaryChange(int[] arr, int n) {
