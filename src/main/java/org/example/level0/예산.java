294ead749893706ec017280cc8446ff1910ba97a
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-08 14:25:20 +0900

    예산

---
+++ src/main/java/org/example/level0/예산.java
@@ -0,0 +1,22 @@
+package org.example.level0;
+
+import java.util.Arrays;
+
+public class 예산 {
+
+    static int solution(int[] d, int budget){
+        int answer =0;
+
+        return answer;
+    }
+    public static void main(String[] args) {
+
+        int[] d = {1,3,2,5,4};
+        int[] d2 = {2,2,3,3};
+        int budget = 9;
+        int budget2= 10;
+
+        System.out.println(solution(d, budget));
+        System.out.println(solution(d2,budget2));
+    }
+}
