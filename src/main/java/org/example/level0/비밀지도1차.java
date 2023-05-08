6e71bc9f491937be784bf24a25438b3d2b110b3c
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-08 17:10:10 +0900

    주어진 매개 배열을 바이너리 배열로 만드는 binaryChange 메소드

--- src/main/java/org/example/level0/비밀지도1차.java
+++ src/main/java/org/example/level0/비밀지도1차.java
@@ -4,14 +4,25 @@ public class 비밀지도1차 {
 
     static String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = {};
-
+        String[] binary_arr1 = binaryChange(arr1,n);
+        for(String s : binary_arr1) {
+            System.out.println(s);
+        }
         return answer;
     }
 
+    static String[] binaryChange(int[] arr, int n) {
+        String[] result = new String[5];
+        for(int i=0; i<result.length; i++) {
+            result[i] = String.format("%05d", Integer.parseInt(Integer.toBinaryString(arr[i])));
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         int n= 5;
         int[] arr1 = {9,20,28,18,11};
         int[] arr2 = {30,1,21,17,28};
+        System.out.println(solution(n,arr1,arr2));
     }
 }
