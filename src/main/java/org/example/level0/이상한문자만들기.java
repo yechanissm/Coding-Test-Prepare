e640f7a179f2a55c17e5e91ce7530f8b285b7624
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-08 12:37:02 +0900

    이상한 문자 만들기

---
+++ src/main/java/org/example/level0/이상한문자만들기.java
@@ -0,0 +1,15 @@
+package org.example.level0;
+
+public class 이상한문자만들기 {
+
+    static String solution(String s) {
+        String answer = "";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String s = "try hello world";
+        System.out.println(solution(s));
+    }
+}
--- src/main/java/org/example/memo.java
+++ src/main/java/org/example/memo.java
@@ -9,10 +9,8 @@ import java.util.stream.Stream;
 
 public class memo {
     public static void main(String[] args) {
-        String s = "dsadasd11";
-        StringBuilder sb = new StringBuilder(s);
-        String s1 = sb.reverse().toString();
-        System.out.println(s1);
+        char a = Character.toUpperCase('a');
+        System.out.println(a);
 
     }
 }
