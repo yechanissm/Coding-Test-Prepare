baf5ce0e10189e4b78295467bb08fb545b67a6bd
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-08 16:53:32 +0900

    [1차] 비밀지도 - 카카오 문제

---
+++ src/main/java/org/example/level0/비밀지도1차.java
@@ -0,0 +1,17 @@
+package org.example.level0;
+
+public class 비밀지도1차 {
+
+    static String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        int n= 5;
+        int[] arr1 = {9,20,28,18,11};
+        int[] arr2 = {30,1,21,17,28};
+    }
+}
