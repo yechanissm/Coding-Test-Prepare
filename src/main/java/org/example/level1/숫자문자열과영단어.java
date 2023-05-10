5b7f302689afba670d52c416075c44c8b98207eb
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-10 13:51:05 +0900

    숫자 문자열과 영단어

---
+++ src/main/java/org/example/level1/숫자문자열과영단어.java
@@ -0,0 +1,15 @@
+package org.example.level1;
+
+public class 숫자문자열과영단어 {
+
+    static int solution(String s) {
+        int answer = 0;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        String s = "one4seveneight";
+
+    }
+}
--- src/main/java/org/example/memo.java
+++ src/main/java/org/example/memo.java
@@ -1,14 +1,12 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class memo {
     public static void main(String[] args) {
-        Arrays.s
+        Map<String, String> map = new HashMap<>();
+        map.
     }
 }
