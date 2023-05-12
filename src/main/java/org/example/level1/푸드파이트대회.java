5134869b3527d6178c24522a523c007b4cfce652
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-12 14:12:43 +0900

    왼쪽 선수가 진행할 방향의 음식 나열하기

--- src/main/java/org/example/level1/푸드파이트대회.java
+++ src/main/java/org/example/level1/푸드파이트대회.java
@@ -3,7 +3,13 @@ package org.example.level1;
 public class 푸드파이트대회 {
 
     static String solution(int[] food) {
-
+        String answer = "";
+        for(int i=1; i<food.length; i++) {
+            int j = food[i]/2;
+            for(int k=0; k<j; k++) {
+                answer += String.valueOf(i);
+            }
+        }
     }
 
     public static void main(String[] args) {
