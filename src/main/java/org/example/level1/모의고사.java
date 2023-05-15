9cbd05fa7bb03cdd5d2bd431636f0cd2f8d177b9
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-15 12:19:10 +0900

    1번, 2번, 3번이 각각 정답을 얼마나 맞췄는지 배열에 담기

--- src/main/java/org/example/level1/모의고사.java
+++ src/main/java/org/example/level1/모의고사.java
@@ -3,7 +3,23 @@ package org.example.level1;
 public class 모의고사 {
 
     static int[] solution(int[] answers){
-        int[] answer = {};
+        int[] answer = new int[3];
+        int[] person1 = {1,2,3,4,5};
+        int[] person2 = {2,1,2,3,2,4,2,5};
+        int[] person3 = {3,3,1,1,2,2,4,4,5,5};
+
+        for(int i=0; i<answers.length; i++) {
+            if(answers[i]==person1[i%5]) {
+                answer[0] ++;
+            }
+            if(answers[i]==person2[i%8]) {
+                answer[1] ++;
+            }
+            if(answers[i]==person3[i%10]) {
+                answer[2] ++;
+            }
+        }
+
         return answer;
     }
     public static void main(String[] args) {
