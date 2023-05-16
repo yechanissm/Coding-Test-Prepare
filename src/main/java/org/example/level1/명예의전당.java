07e956ade59f9528d5222e51b9e143d24c7fbe0c
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-16 11:58:41 +0900

    명예의 전당

---
+++ src/main/java/org/example/level1/명예의전당.java
@@ -0,0 +1,20 @@
+package org.example.level1;
+
+import java.util.Arrays;
+
+public class 명예의전당 {
+
+    static int[] solution(int k, int[] score) {
+        int[] answer = {};
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] score = {10,100,20,150,1,100,200};
+        int k = 3;
+
+        System.out.println(Arrays.toString(solution(k, score)));
+
+    }
+}
--- src/main/java/org/example/memo.java
+++ src/main/java/org/example/memo.java
@@ -9,18 +9,33 @@ import java.util.stream.Stream;
 
 public class memo {
     public static void main(String[] args) {
-        String substring = LocalDate.of(2016, 5, 24).getDayOfWeek().toString().substring(0, 3);
-        System.out.println(substring);
+        import java.util.*;
 
-        String[][] photo = {{"may","kevin","kain","radi"},
-                {"may","kevin","fsd","dsfs"},
-                {"kon", "kain","may","coni"}};
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(1,5);
-        map.put(2,0);
-        map.put(3,0);
+        class Solution {
+            public int[] solution(int k, int[] score) {
+                int[] answer = new int[score.length];
+                List<Integer> list = new ArrayList<>();
+                for(int i=0; i<score.length; i++) {
+                    if(list.size() < k) {
+                        list.add(score[i]);
+                        Collections.sort(list);
+                        answer[i] = list.get(0);
+                        continue;();
+                    }
+                    if(list.size() == k) {
+                        int min = list.get(0);
+                        if(min < score[i]) {
+                            list.remove(0);
+                            list.add(score[i]);
+                            Collections.sort(list);
+                        }
+                        answer[i] = list.get(0);
+                    }
+                }
 
-        System.out.println(map);
+                return answer;
+            }
+        }
 
 
 
