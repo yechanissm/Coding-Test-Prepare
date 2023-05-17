35044fc9acb1a63a421856e6cbc53fffad7c018e
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-17 18:12:43 +0900

    정답 - 명예의 전당

--- src/main/java/org/example/level1/명예의전당.java
+++ src/main/java/org/example/level1/명예의전당.java
@@ -1,11 +1,31 @@
 package org.example.level1;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class 명예의전당 {
 
     static int[] solution(int k, int[] score) {
-        int[] answer = {};
+        int[] answer = new int[score.length];
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<score.length; i++) {
+            if(list.size()<k) {
+                list.add(score[i]);
+                Collections.sort(list);
+                answer[i] = list.get(0);
+            }
+            else if(list.size()==k) {
+                int low = list.get(0);
+                if(low < score[i]) {
+                    list.remove(0);
+                    list.add(score[i]);
+                    Collections.sort(list);
+                }
+                answer[i] = list.get(0);
+            }
+        }
 
         return answer;
     }
--- src/main/java/org/example/memo.java
+++ src/main/java/org/example/memo.java
@@ -8,34 +8,11 @@ import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class memo {
+
     public static void main(String[] args) {
-        import java.util.*;
 
-        class Solution {
-            public int[] solution(int k, int[] score) {
-                int[] answer = new int[score.length];
-                List<Integer> list = new ArrayList<>();
-                for(int i=0; i<score.length; i++) {
-                    if(list.size() < k) {
-                        list.add(score[i]);
-                        Collections.sort(list);
-                        answer[i] = list.get(0);
-                        continue;();
-                    }
-                    if(list.size() == k) {
-                        int min = list.get(0);
-                        if(min < score[i]) {
-                            list.remove(0);
-                            list.add(score[i]);
-                            Collections.sort(list);
-                        }
-                        answer[i] = list.get(0);
-                    }
-                }
 
-                return answer;
-            }
-        }
+
 
 
 
