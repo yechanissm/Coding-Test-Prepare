934d5b55cc7a74a28f041b9c7fde36a189431f79
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-05-25 19:43:46 +0900

    소수 찾기 문제  - 효율성 중요

---
+++ src/main/java/org/example/level1/소수찾기.java
@@ -0,0 +1,12 @@
+package org.example.level1;
+
+public class 소수찾기 {
+    static int solution(int n) {
+
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(solution(n));
+    }
+}
