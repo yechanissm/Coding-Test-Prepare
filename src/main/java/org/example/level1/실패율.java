a58aee0520c2a5728a56f3a276d66d646c956c03
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-06-02 19:19:33 +0900

    실패율

---
+++ src/main/java/org/example/level1/실패율.java
@@ -0,0 +1,16 @@
+package org.example.level1;
+
+public class 실패율 {
+
+    static int[] solution(int N, int[] stages) {
+
+    }
+
+    public static void main(String[] args) {
+        int N = 5;
+        int[] stages = {3,4,2,1,5};
+        int[] stages2 = {3,3,3,3};
+
+
+    }
+}
