27ed80eeeddf07260d65333b2ee3a4d127aaad4c
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-06-02 16:52:43 +0900

    괄호 변환 코드 수정

--- src/main/java/org/example/level2/카카오/괄호변환.java
+++ src/main/java/org/example/level2/카카오/괄호변환.java
@@ -5,71 +5,42 @@ import java.util.Stack;
 public class 괄호변환 {
 
     public static String solution(String p) {
-        String answer = "";
-        if(isCorrect(p)) {
-            return p;
-        }
-        answer = dps(p);
-        return answer;
+        return go(p);
     }
-    static boolean isCorrect(String p) {
-        Stack<Character> st  = new Stack<>();
+
+    private static String go(String p) {
+        if(p.equals("")) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder(p);
+        String[] tmp = splitString(p);
+        String u = tmp[0];
+        String v = tmp[1];
+
+        return u;
+    }
+
+    private static String[] splitString(String p) {
+        int key = 0;
         for(int i=0; i<p.length(); i++) {
             if(p.charAt(i)=='(') {
-                st.push('(');
+                key++;
             }
-            else{
-                if(st.isEmpty() || st.peek()==')'){
-                    return false;
+            else {
+                key--;
+            }
+            if(key==0) {
+                String u = p.substring(0,i+1);
+                String v = "";
+                if(p.length()>= i+1){
+                    System.out.println(p.length());
+                    System.out.println(p);
+                    v = p.substring(i+1);
                 }
-                else {
-                    st.pop();
-                }
+                return new String[] {u,v};
             }
         }
-        return true;
-    }
-
-    static String dps(String w) {
-        if (w.length() == 0) {
-            return w;
-        }
-        String u = "";
-        String v = "";
-        String str = "";
-        int check = 0;
-
-        for (int i = 0; i < w.length(); i++) {
-            if (w.charAt(0) == '(') {
-                check++;
-            } else {
-                check--;
-            }
-            if (check == 0) {
-                u += w.substring(0, i + 1);
-                v += w.substring(i + 1);
-                break;
-            }
-        }
-
-        if (isCorrect(u)) {
-            return u + dps(v);
-        } else {
-            String tmp = "(";
-            tmp += dps(v);
-            tmp += ")";
-
-            u = u.substring(1, u.length() - 1);
-
-            for (int i = 0; i < u.length(); i++) {
-                if (u.charAt(i) == '(') {
-                    tmp += ')';
-                } else {
-                    tmp += '(';
-                }
-            }
-            return tmp;
-        }
+        return new String[] {"", ""};
     }
 
     public static void main(String[] args) {
@@ -77,6 +48,7 @@ public class 괄호변환 {
         String p2 = ")(";
         String p3 = "()))((()";
 
+        System.out.println(solution(p));
         System.out.println(solution(p2));
         System.out.println(solution(p3));
     }
