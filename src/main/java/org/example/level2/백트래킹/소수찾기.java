f1d6237aea5e85723361797a2c0495224e0b9a0d
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-07-03 18:22:52 +0900

    소수 찾기 - 완전 탐색

---
+++ src/main/java/org/example/level2/백트래킹/소수찾기.java
@@ -0,0 +1,40 @@
+package org.example.level2.백트래킹;
+
+import java.util.HashSet;
+
+public class 소수찾기 {
+
+    static HashSet<Integer> set = new HashSet<>();
+    static char[] arr;
+    static boolean[] visited;
+
+    static int solution(String numbers) {
+        int answer = 0;
+        arr = new char[numbers.length()];
+        visited = new boolean[numbers.length()];
+
+        for(int i=0; i<numbers.length(); i++) {
+            arr[i] = numbers.charAt(0);
+        }
+        recursion("", 0);
+
+
+        return answer;
+    }
+
+    private static void recursion(String str, int index) {
+        int num;
+        if(str!="") {
+            num = Integer.parseInt(str);
+            if(isPrime)
+        }
+    }
+
+    public static void main(String[] args) {
+        String numbers1 = "17";
+        String numbers2 = "011";
+
+        System.out.println(solution(numbers1));
+        System.out.println(solution(numbers2));
+    }
+}
--- src/main/java/org/example/memo2.java
+++ src/main/java/org/example/memo2.java
@@ -1,24 +1,16 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class memo2 {
 
 
     public static void main(String[] args) {
-        String s =  "(){}[]";
-        String tmp = s;
-        int answer = 0;
-        for(int i=0; i<s.length(); i++) {
-            char c1 = s.charAt(0);
-            tmp = s.substring(1);
-            tmp += c1;
-            s = tmp;
-            for(int j=0; j<s.length()/2; j++) {
-                tmp = tmp.replace("()", "");
-                tmp = tmp.replace("[]", "");
-                tmp = tmp.replace("{}", "");
-            }
-            if(tmp.equals("")){answer++;}
+        List<Integer> list = new ArrayList;
+
+        for (Integer integer : list) {
+            
         }
-        System.out.println(answer);
     }
 }
