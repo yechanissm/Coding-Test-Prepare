21d92b0afa6e10cbfa8d4b7491b958286a9778ed
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-10-10 12:02:33 +0900

    홀수 더하기 문제

---
+++ src/main/java/org/example/difficult1/홀수더하기.java
@@ -0,0 +1,17 @@
+package org.example.difficult1;
+
+import java.util.Scanner;
+
+public class 홀수더하기 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+
+        for(int test_case = 1; test_case <= T; test_case++)
+        {
+
+        }
+    }
+}
--- src/main/java/org/example/memo.java
+++ src/main/java/org/example/memo.java
@@ -1,49 +1,20 @@
 package org.example;
 
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.LongStream;
-import java.util.stream.Stream;
+class Parent {
+    public void show() {
+        System.out.println("parent");
+    }
+}
+
+class Child extends Parent {
+    public void show() {
+        System.out.println("child");
+    }
+}
 
 public class memo {
-    public static int[] solution(int N, int[] stages) {
-        int[] answer = new int[N];
-        Map<Integer, Double> map = new HashMap<>();
-        int person = stages.length;
-        double rate = 0;
-        int stage = 1;
-        int up = 0;
-        for(int i=1; i<=N; i++) {
-            for(int j=0; j<stages.length; j++) {
-                if(stages[j]==stage) {
-                    up++;
-                }
-            }
-            rate = (double) up/person;
-            person -=up;
-            up = 0;
-            stage++;
-            map.put(i, rate);
-        }
-        List<Integer> key = new ArrayList<>(map.keySet());
-
-        Collections.sort(key, (v1,v2) -> (map.get(v2).compareTo(map.get(v1))));
-
-        for(int k :key) {
-            System.out.println(k);
-        }
-
-        return answer;
-    }
-
     public static void main(String[] args) {
-        int N = 5;
-        int[] stages = {3,3,3,3};
-
-        System.out.println(solution(N,stages));
-
+        Parent pa = new Child();
+        pa.show();
     }
 }
