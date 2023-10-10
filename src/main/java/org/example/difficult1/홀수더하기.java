844804ed5a17ea38f1e1cc23dc36cd2926c6581d
yechanissm <106381504+yechanissm@users.noreply.github.com>
2023-10-10 12:20:01 +0900

    홀수 더하기 정답

--- src/main/java/org/example/difficult1/홀수더하기.java
+++ src/main/java/org/example/difficult1/홀수더하기.java
@@ -11,7 +11,17 @@ public class 홀수더하기 {
 
         for(int test_case = 1; test_case <= T; test_case++)
         {
-
+            int[] array = new int[10];
+            int sum =0;
+            for(int i=0; i<10; i++) {
+                array[i]  = sc.nextInt();
+            }
+            for(int i=0; i<10; i++) {
+                if(array[i]%2==1) {
+                    sum+= array[i];
+                }
+            }
+            System.out.println("#"+ test_case + " " + sum);
         }
     }
 }
